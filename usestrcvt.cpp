ab6a84f48098622eaf57841656355977f2a03552
sjuda <32189174+sjuda@users.noreply.github.com>
2018-09-06 17:21:16 +0300

    Create usestrcvt.cpp

---
+++ usestrcvt.cpp
@@ -0,0 +1,14 @@
+int main() {
+
+try
+	{
+		printf("%f\n", FromString<float>("-1,6251"));
+	}
+	catch (const std::exception& e)
+	{
+		cout << e.what() << endl;
+	}
+
+//	system("pause");
+    return 0;
+}
